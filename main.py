fe258a092ae3094eb14b13d96a3997e3c6bf42f6
DarkTrojan-MJTE <82009182+Dark-Trojan-MjtE@users.noreply.github.com>
2021-06-09 08:01:28 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import requests as req
+import time
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+@app.route('/response', methods=['POST'])
+def resultado():
+
+    cel = request.form['celular']
+    texto = request.form['mensaje']
+
+
+    resp = req.post('https://textbelt.com/text', {
+        'phone': '{}'.format(cel),
+        'message': '{}'.format(texto),
+        'key': 'textbelt',
+        })
+
+    resp_end = resp.json()
+    fecha = time.strftime('%d/%m/%Y')
+    hora = time.strftime('%I:%M:%S %p')
+
+    return render_template('resultado.html',
+            cel = cel,
+            respuesta = resp_end['success'],
+            fecha = fecha,
+            hora = hora,
+            )
+
+if __name__ == '__main__':
+    app.run(debug=True)
