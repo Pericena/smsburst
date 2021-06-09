10a2be5b63afdaedb268fe3e4c640cff5c4f1af9
DarkTrojan-MJTE <82009182+Dark-Trojan-MjtE@users.noreply.github.com>
2021-06-09 15:48:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 
+import os, platform
+import webbrowser as web
+import subprocess as subp
+from time import sleep
+
+
+if platform.python_version() >= '3':
+    pass
+else:
+    print('Soporta con python3 :)')
+    exit()
+
+if os.path.exists('/usr/'):
+    print('Ejecutando en Linux...')
+    sleep(1.5)
+    print('Iniciando Servidor Flask...')
+    sleep(2)
+    os.system('python3 smsburst.py')
+
+elif os.path.exists(r'C:\Users'):
+    print('Ejecutando en Windows...')
+    sleep(1.5)
+    print('Iniciando Servidor Flask...')
+    sleep(2)
+    os.system('python smsburst.py')
+    
+elif os.path.exists('/data/data/com.termux/files/home'):
+    print('Ejecutando en Termux...')
+    sleep(1.5)
+    print('Iniciando Servidor Flask...')
+    sleep(2)
+    os.system('python3 smsburst.py')
